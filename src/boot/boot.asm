7fddb4675cced3ec49ee1acf5791949b1cffa22a
xingguosimida <xingguosimida@gmail.com>
2023-07-09 00:02:42 +0800

    Load the kernel to memory

--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -16,11 +16,6 @@ step2:
     mov sp, 0x7c00
     sti ; Enables Interrupts
 
-A20:
-    in al, 0x92
-    or al, 2
-    out 0x92, al
-    ret
 
 enter_protected_mode:
     cli
@@ -34,16 +29,70 @@ enter_protected_mode:
 
 [BITS 32]
 load32:
-    mov ax, DATA_SEG
-    mov ds, ax
-    mov es, ax
-    mov fs, ax
-    mov gs, ax
-    mov ss, ax
-    mov ebp, 0x00200000
-    mov esp, ebp
-    jmp A20
-    jmp $
+    mov eax, 1
+    mov ecx, 100
+    mov edi, 0x0100000
+    call ata_lba_read
+    jmp CODE_SEG:0x0100000
+    
+ata_lba_read:
+    pushfd
+    and eax, 0x0FFFFFFF
+    push eax
+    push ebx
+    push ecx
+    push edx
+    push edi
+ 
+    mov ebx, eax         ; Save LBA in EBX
+ 
+    mov edx, 0x01F6      ; Port to send drive and bit 24 - 27 of LBA
+    shr eax, 24          ; Get bit 24 - 27 in AL
+    or al, 11100000b     ; Set bit 6 in AL for LBA mode
+    out dx, al
+ 
+    mov edx, 0x01F2      ; Port to send number of sectors
+    mov al, cl           ; Get number of sectors from CL
+    out dx, al
+ 
+    mov edx, 0x1F3       ; Port to send bit 0 - 7 of LBA
+    mov eax, ebx         ; Get LBA from EBX
+    out dx, al
+ 
+    mov edx, 0x1F4       ; Port to send bit 8 - 15 of LBA
+    mov eax, ebx         ; Get LBA from EBX
+    shr eax, 8           ; Get bit 8 - 15 in AL
+    out dx, al
+ 
+    mov edx, 0x1F5       ; Port to send bit 16 - 23 of LBA
+    mov eax, ebx         ; Get LBA from EBX
+    shr eax, 16          ; Get bit 16 - 23 in AL
+    out dx, al
+ 
+    mov edx, 0x1F7       ; Command port
+    mov al, 0x20         ; Read with retry.
+    out dx, al 
+
+.still_going:
+    in al, dx
+    test al, 8           ; the sector buffer requires servicing.
+    jz .still_going      ; until the sector buffer is ready.
+ 
+    mov eax, 256         ; to read 256 words = 1 sector
+    xor bx, bx
+    mov bl, cl           ; read CL sectors
+    mul bx
+    mov ecx, eax         ; ECX is counter for INSW
+    mov edx, 0x1F0       ; Data port, in and out
+    rep insw             ; in to [EDI]
+ 
+    pop edi
+    pop edx
+    pop ecx
+    pop ebx
+    pop eax
+    popfd
+    ret
 
 gdt_start:
 
@@ -77,6 +126,7 @@ gdt_end:
 gdt_descriptor:
     dw gdt_end - gdt_start - 1 ;因为开始索引是0，所以这里需要减1                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
     dd gdt_start
+    
 
 times 510-($ - $$) db 0
 
