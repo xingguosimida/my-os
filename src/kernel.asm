f1cc363d6b512936b00fea9975f408be0039d30c
xingguosimida <xingguosimida@gmail.com>
2023-07-20 21:42:21 +0800

    Implement the PIC

--- src/kernel.asm
+++ src/kernel.asm
@@ -17,11 +17,21 @@ _start:
     in al, 0x92
     or al, 2
     out 0x92, al
+    ; PIC 开始 参考 https://en.wikibooks.org/wiki/X86_Assembly/Programmable_Interrupt_Controller
+    mov al, 00010001b
+    out 0x20, al ; Tell master PIC
+
+    mov al, 0x20 ; Interrupt 0x20 is where master ISR should start
+    out 0x21, al
+
+    mov al, 00000001b
+    out 0x21, al
+    sti
+    ; PIC 结束
+
     call kernel_main
     jmp $
 
-problem:
-    mov eax, 0
-    div eax
+
 
 times 512-($ - $$) db 0
